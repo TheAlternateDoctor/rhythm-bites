141ee5ce88f028622ca4b7807022a1a3c2d634ff
Mathys Ballagny <contact@mathysballagny.fr>
2022-03-13 20:52:04 +0100

    i should make more of these does a bunch of stuff ig

--- compileFork.ps1
+++ compileFork.ps1
@@ -1,3 +1,3 @@
 ﻿java8 -jar ..\tickompiler.jar c tickflow\ bin\
-java8 -jar ..\tickompiler.jar p bin\ ..\code.bin
+java8 -jar ..\tickompiler.jar p bin\ base.bin
 mv -force C00.bin $Env:APPDATA\Citra\sdmc\rhmm
