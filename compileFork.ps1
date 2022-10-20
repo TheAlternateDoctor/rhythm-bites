9220e9c05e8c0f5a391f1f3f70a59537a0db7fa9
TheAlternateDoctor <thealternatedoc@gmail.com>
2022-10-21 00:20:27 +0200

    Tweaked a lot of shorts in small ways Added 2 new shorts, The Lick and Klonoa Heroes Made textures for Tap Trial & Packing Pests

--- compileFork.ps1
+++ compileFork.ps1
@@ -1,3 +1,3 @@
 ﻿java8 -jar ..\tickompiler.jar c tickflow\ bin\
-java8 -jar ..\tickompiler.jar p bin\ base.bin
+java8 -jar ..\tickompiler.jar p bin\ shorts.bin
 mv -force C00.bin $Env:APPDATA\Citra\sdmc\rhmm
